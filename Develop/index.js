b6e1ae1d0e6a437fe66ea796ebce36df8b49ebbb
saveetha17 <saveethakathiravan@gmail.com>
2023-05-26 15:26:11 -0400

    worked on Index and Generate Markdown file

---
+++ Develop/index.js
@@ -0,0 +1,75 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter your Project Description",
+  },
+  {
+    type: "input",
+    name: "tableOfContents",
+    message: "Type in Table of Contents",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the Installation procedure",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Explain how to use your project",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Mention your License to the project",
+    choices: ["MIT", "GNU", "Apache", "None"],
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Who are all contributed to your project",
+  },
+  {
+    type: "input",
+    name: "Tests",
+    message: "Explain how to test/run your project",
+  },
+  {
+    type: "list",
+    name: "questions",
+    message:
+      "What is your medium of communication to reach you with questions?",
+    choices: ["Github", "E-mail"],
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Successfully created readme.md!")
+  );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const readMePageContent = generateMarkdown(answers);
+
+    writeToFile("readme.md", readMePageContent);
+  });
+}
+
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,62 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return "You have a license!";
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return "You have a license!";
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents (Optional)
+
+${data.tableOfContents}
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+## License
+
+${data.license}
+
+`;
+}
+
+module.exports = generateMarkdown;
